13b8f4b89c7a595661d9e72c086217307699ded7
sico22 <39299882+sico22@users.noreply.github.com>
2022-04-15 10:10:02 +0300

    Add files via upload

---
+++ package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"businessLogic"},{"l":"connection"},{"l":"dataAccess"},{"l":"model"},{"l":"presentation"},{"l":"start"},{"l":"validators"}];updateSearchResults();
